4dcc7a7cede2b58e0aa4b4d36e4252c9552fbc48
saynschuman <saynschuman@gmail.com>
2017-05-08 16:04:31 +0300

    finished

--- dist/js/main.js
+++ dist/js/main.js
@@ -8,4 +8,15 @@
 	  	autoplaySpeed: 3000
 	})
 
+	$('.sh-m').click(function(){
+		$('.text-inner').addClass('norm-size');
+		$(this).hide();
+	})
+
+	$('.up').click(function(){
+		$('body,html').animate({
+				scrollTop: 0
+			}, 800);
+	})
+
 })(jQuery);
